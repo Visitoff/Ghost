0a327719c7b4d59955fb01d0d8dad6f37fcb089e
Visitoff <60969574+Visitoff@users.noreply.github.com>
2020-02-19 16:53:41 +0500

    Ghost

---
+++ Assets/Scripts/GhostInstanitate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostInstanitate : MonoBehaviour
+{
+    GameObject GhostPrefab;
+    public Animation anim;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+        anim.Play();
+    }
+
+   
+}
