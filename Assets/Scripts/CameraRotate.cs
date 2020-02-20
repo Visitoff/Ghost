442152d1f3d54fa2146eb7866114a101b4dff006
Visitoff <60969574+Visitoff@users.noreply.github.com>
2020-02-20 16:28:56 +0500

    Camera fix1.2

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -7,11 +7,17 @@ public class CameraRotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Input.gyro.enabled = true;
-        if (Application.isMobilePlatform)
         
-        Debug.Log("1");
+        if (Application.isMobilePlatform)
+        {
+            GameObject cameraParent = new GameObject("camParent");
+            cameraParent.transform.position = this.transform.position;
+            this.transform.parent = cameraParent.transform;
+            cameraParent.transform.Rotate(Vector3.right, 90); //This is for rotation of camera.
+        }
         Input.gyro.enabled = true;
+        Debug.Log("1");
+      
     }
 
     // Update is called once per frame
@@ -19,7 +25,7 @@ public class CameraRotate : MonoBehaviour
     {
         Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y,
             -Input.gyro.attitude.z, -Input.gyro.attitude.w);
-        transform.localRotation = cameraRotation;
+        this.transform.localRotation = cameraRotation;
         
     }
 }
