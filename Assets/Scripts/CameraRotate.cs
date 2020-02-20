4e4d96617120fa521372e87866bb544bae01c6c2
Visitoff <60969574+Visitoff@users.noreply.github.com>
2020-02-20 16:36:28 +0500

    Update CameraRotate.cs

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     void Start()
     {
         
@@ -13,14 +13,14 @@ public class CameraRotate : MonoBehaviour
             GameObject cameraParent = new GameObject("camParent");
             cameraParent.transform.position = this.transform.position;
             this.transform.parent = cameraParent.transform;
-            cameraParent.transform.Rotate(Vector3.right, 90); //This is for rotation of camera.
+            cameraParent.transform.Rotate(Vector3.right, 90); 
         }
         Input.gyro.enabled = true;
         Debug.Log("1");
       
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y,
