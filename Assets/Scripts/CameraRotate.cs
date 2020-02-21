18d30783c8fe595895690a22a23567d056a7551d
Visitoff <60969574+Visitoff@users.noreply.github.com>
2020-02-21 16:15:16 +0500

    Ghost_fix1.0

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -3,12 +3,13 @@
 public class CameraRotate : MonoBehaviour
 {   
     void Start()
-    {  if (Application.isMobilePlatform)
+    {  
+        if (Application.isMobilePlatform)
         {
             GameObject cameraParent = new GameObject("camParent");
             cameraParent.transform.position = this.transform.position;
             this.transform.parent = cameraParent.transform;
-            cameraParent.transform.Rotate(Vector3.right, 90); 
+            cameraParent.transform.Rotate(Vector3.right, 90 * Time.deltaTime); 
         }
         Input.gyro.enabled = true;
     }
@@ -16,7 +17,7 @@ public class CameraRotate : MonoBehaviour
     void Update()
     {
         Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y,
-        -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+        -Input.gyro.attitude.z, -Input.gyro.attitude.w * Time.deltaTime);
         this.transform.localRotation = cameraRotation;
     }
 }
