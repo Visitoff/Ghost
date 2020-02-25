a3c048d9d4befc4395392920fe7ad21c29fdc5e9
Visitoff <60969574+Visitoff@users.noreply.github.com>
2020-02-25 17:04:27 +0500

    DeltaFix

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -1,23 +1,34 @@
 ﻿using UnityEngine;
 
 public class CameraRotate : MonoBehaviour
-{   
-    void Start() 
-    {  
+{
+    float clampDistance = Screen.width * 0.05f;
+    void Start()
+    {
         if (Application.isMobilePlatform)
         {
             GameObject cameraParent = new GameObject("camParent");
             cameraParent.transform.position = this.transform.position;
             this.transform.parent = cameraParent.transform;
-            cameraParent.transform.Rotate(Vector3.right, 90); 
+            cameraParent.transform.Rotate(Vector3.right, 90);
         }
         Input.gyro.enabled = true;
     }
 
     void Update()
     {
+        Quaternion zeroZone = new Quaternion(0f, 0f, 0f, 0f);
         Quaternion cameraRotation = new Quaternion(Input.gyro.attitude.x * Time.deltaTime, Input.gyro.attitude.y * Time.deltaTime,
         -Input.gyro.attitude.z * Time.deltaTime, -Input.gyro.attitude.w * Time.deltaTime);
-        this.transform.localRotation = cameraRotation ;
+        this.transform.localRotation = cameraRotation;
+        Debug.Log(cameraRotation);
+        if (cameraRotation = 0f) 
+        {
+
+        }
+        Quaternion cameraRotation { get; }
+
     }
 }
+    
+ 
